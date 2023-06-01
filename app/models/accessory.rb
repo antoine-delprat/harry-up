6f75db1523ed31110f64ec296223ae2822015abb
Antoine Delprat <antoine.delprat.pro@gmail.com>
2023-06-01 18:04:40 +0200

    creating form new accessory and link in navbar

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,4 +1,10 @@
 class Accessory < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_one_attached :photo
+  
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :price_per_day, presence: true
+  validates :category, presence: true
+  validates :photo, presence: true
 end
