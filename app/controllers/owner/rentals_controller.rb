506b52e45ff2cc7a5660737a4e2eb5adf7d695c1
matthieu boussicault <boussicaultmatthieu@gmail.com>
2023-06-01 17:57:54 +0200

    my-demands accept and decline

--- app/controllers/owner/rentals_controller.rb
+++ app/controllers/owner/rentals_controller.rb
@@ -1,6 +1,21 @@
 class Owner::RentalsController < ApplicationController
 
   def index
-    @rentals = Rental.includes(:accessory).where(accessories: { owner: current_user })
+    @rentals_pending = Rental.includes(:accessory).where(accessories: { owner: current_user }, status: "Pending")
+    @rentals_accepted = Rental.includes(:accessory).where(accessories: { owner: current_user }, status: "Accepted")
+    @rentals_rejected = Rental.includes(:accessory).where(accessories: { owner: current_user }, status: "Rejected")
+    @rentals = @rentals_pending + @rentals_accepted + @rentals_rejected
+  end
+
+  def accept
+    @rental = Rental.find(params[:id])
+    @rental.update(status: "Accepted")
+    redirect_to owner_rentals_path
+  end
+
+  def decline
+    @rental = Rental.find(params[:id])
+    @rental.update(status: "Rejected")
+    redirect_to owner_rentals_path
   end
 end
