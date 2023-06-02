5feb5ab9bc61fcdc7ebe0dcb984d092b87358fd4
matthieu boussicault <boussicaultmatthieu@gmail.com>
2023-06-02 13:01:13 +0200

    accept decline design

--- app/controllers/owner/rentals_controller.rb
+++ app/controllers/owner/rentals_controller.rb
@@ -1,4 +1,5 @@
 class Owner::RentalsController < ApplicationController
+  before_action :set_rental, only: [:accept, :decline]
 
   def index
     @rentals_pending = Rental.includes(:accessory).where(accessories: { owner: current_user }, status: "Pending")
@@ -8,14 +9,18 @@ class Owner::RentalsController < ApplicationController
   end
 
   def accept
-    @rental = Rental.find(params[:id])
     @rental.update(status: "Accepted")
     redirect_to owner_rentals_path
   end
 
   def decline
-    @rental = Rental.find(params[:id])
     @rental.update(status: "Rejected")
     redirect_to owner_rentals_path
+  end
+
+  private
+
+  def set_rental
+    @rental = Rental.find(params[:id])
   end
 end
